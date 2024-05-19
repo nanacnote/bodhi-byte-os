29cdb9add2719ac9e7f0056a68e61a0ba08bf4ba
nanacnote <adjeibohyen@hotmail.co.uk>
2024-05-19 17:10:04 +0100

    extend login env variables

--- dotfiles/script/on-login-script.sh
+++ dotfiles/script/on-login-script.sh
@@ -1,6 +1,10 @@
 # environment vars
+export XDG_CONFIG_HOME="${HOME}/.config"
+export EDITOR=vim
+
 export PATH="${HOME}/.local/bin:${PATH}"      # pip install <pkg> --user  # user flag installs bin here (eg. pipenv, tox etc)
 export PIPENV_VENV_IN_PROJECT=1               # forces pipenv to use "the-projects-root/.venv" to store venv
+
 
 # start dwm
 startx
