e958786b72b9fcd9e163428019f4550c6c1f06c3
nanacnote <adjeibohyen@hotmail.co.uk>
2024-06-09 12:05:14 +0100

    a secondary fonts to st

--- st/config.h
+++ st/config.h
@@ -6,6 +6,10 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "FiraMono Nerd Font:style=Medium:pixelsize=16:antialias=true:autohint=true";
+/* Spare fonts */
+static char *font2[] = {
+	"Noto Color Emoji:pixelsize=16:antialias=true:autohint=true",
+};
 static int borderpx = 2;
 
 /*
