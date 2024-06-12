c555c2a2f3ab5d0eb9d6542bc28c3bf0f225eb27
nanacnote <adjeibohyen@hotmail.co.uk>
2024-06-12 22:51:10 +0100

    add vi mode patch for dmenu

--- dmenu/config.h
+++ dmenu/config.h
@@ -7,7 +7,8 @@ static int centered = 1;                    /* -c option; centers dmenu on scree
 static int min_width = 750;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"FiraMono Nerd Font:style=Regular:size:10"
+	"FiraMono Nerd Font:style=Regular:size:10",
+	"Noto Color Emoji:style=Regular:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
@@ -15,6 +16,7 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
 	[SchemeSel] = { "#eeeeee", "#458588" },
 	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeCursor] = { "#222222", "#bbbbbb"},
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 12;
@@ -24,6 +26,18 @@ static unsigned int lines      = 12;
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+
+/*
+ * -vi option; if nonzero, vi mode is always enabled and can be
+ * accessed with the global_esc keysym + mod mask
+ */
+static unsigned int vi_mode = 1;
+static unsigned int start_mode = 0;			/* mode to use when -vi is passed. 0 = insert mode, 1 = normal mode */
+static Key global_esc = { XK_n, ControlMask };	/* escape key when vi mode is not enabled explicitly */
+static Key quit_keys[] = {
+	/* keysym	modifier */
+	{ XK_q,		0 }
+};
 
 /* Size of the window border */
 static unsigned int border_width = 3;
